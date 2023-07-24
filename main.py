baba4ff513a0d27f67317069104eceb20c9dad29
guiltedrose <guiltedrose@proton.me>
2023-07-23 23:13:53 -0400

    Updated main.py with first real function

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 #!/bin/python
-print("hello world, I am BMO!\n Beep.\n Boop.\n")
+# imports
+
+import os
+
+# This is the path for the modules the AI has access to.
+path = r'tools'
+
+if os.getcwd() != path and path == False:
+    os.mkdir(path)
+    os.chdir(path)
+elif os.getcwd() != path and path == True:
+    os.chdir(path)
+else:
+    os.chdir(path)
+
+print(os.getcwd())
