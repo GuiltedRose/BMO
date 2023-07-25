535bd3d8a7decaf8fdfe4841dcab1fb14b4fe520
guiltedrose <guiltedrose@proton.me>
2023-07-24 22:11:39 -0400

    Updated main.py

--- main.py
+++ main.py
@@ -2,16 +2,29 @@
 # imports
 
 import os
+import sys
 
 # This is the path for the modules the AI has access to.
 path = r'tools'
 
-if os.getcwd() != path and path == False:
+try:
     os.mkdir(path)
-    os.chdir(path)
-elif os.getcwd() != path and path == True:
-    os.chdir(path)
-else:
-    os.chdir(path)
+    print("Directory for tools created.")
+except OSError:
+    if os.path.exists(path):
+        print("This directory already exists.")
+    else:
+        sys.exit("Error with file permissions, this user cannot access this directory")
+print("Would you like to go into the 'tools' directory?\n")
+answer = input("Y/n\n")
 
-print(os.getcwd())
+yes_choices = ["yes", "y"]
+no_choices = ["no", "n"]
+
+if answer.lower() in yes_choices:
+    os.chdir(path)
+    print(os.getcwd())
+elif answer.lower() in no_choices:
+    print("Okie Then...")
+else:
+    print("It's yes or no...")
