ca85652783c0a145e1d456c6a0fd0cd07bb6d6b9
guiltedrose <guiltedrose@proton.me>
2023-07-29 19:04:23 -0400

    Added a file created via main.py

--- main.py
+++ main.py
@@ -25,8 +25,36 @@ no_choices = ["no", "n"]
 if answer.lower() in yes_choices:
     os.chdir(path)
     print(os.getcwd())
+    file = "scanner.py"
+    try: 
+        open(file, "x")
+    except OSError:
+        os.path.exists(file)
+        print("This file exists, should we append the contents?")
+        answer = input("y/n\n")
+        if answer.lower() in yes_choices:
+            with open(file, "w") as f:
+                data = "print('hello world')"
+                f.write(data)
 elif answer.lower() in no_choices:
-    print("Okie Then...")
+    print("Okie, Would you like to list the contents instead?")
+    answer = input("y/n\n")
+    if answer.lower() in yes_choices:
+        os.getcwd(path)
+    elif answer.lower() in no_choices:
+        print("Okie, would you like to exit?")
+        answer = input("y/n\n")
+        if answer.lower() in yes_choices:
+            sys.exit()
 else:
     print("It's yes or no...")
+    try:
+        with open(file, "w") as f:
+            data = ("print('hello world')")
+            f.write(data)
+
+    except OSError:
+        if os.path.exists(file):
+            print("This file exists, would you like to run it?")
+
 
